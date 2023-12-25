06425d19b5e3f53c0c6c9ab9c93f457188ad5db2
Mr-PangHu <63227347+Mr-PangHu@users.noreply.github.com>
2023-12-25 16:26:33 +0800

    数据库连接修改

--- src/server/db2/index.js
+++ src/server/db2/index.js
@@ -1,11 +1,13 @@
 let mysql = require('mysql')
 
 let db = mysql.createConnection({
-  // host: '172.20.137.141', // 数据库IP地址
-  host: '127.0.0.1', // 数据库IP地址
+  host: '172.20.137.141', // 数据库IP地址
+  // host: '127.0.0.1', // 数据库IP地址
   user: 'root', // 数据库登录账号
-  password: 'admin123', // 数据库登录密码
-  database: 'olympic_all', // 要操作的数据库
+  // password: 'admin123', // 数据库登录密码
+  password: '123456', // 数据库登录密码
+  // database: 'olympic_all', // 要操作的数据库
+  database: 'persona_test', // 要操作的数据库
   port: '3306'
 })
 
