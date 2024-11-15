b420a772b14a408683ed29abf31e28c879bbe4b7
xiehou-design <duanzhenzhen163@163.com>
2024-11-15 15:29:27 +0800

    更新成绩预测和疲劳预测的显示和数据库连接情况,切换回模拟数据库

--- src/server/db/index.js
+++ src/server/db/index.js
@@ -1,17 +1,14 @@
 let mysql = require('mysql')
 
-// let db = mysql.createPool({
-//   host: '127.0.0.1', // 数据库IP地址
-//   user: 'root', // 数据库登录账号
-//   password: 'admin123', // 数据库登录密码
-//   database: 'olympic' // 要操作的数据库
-// })
-
-let db = mysql.createConnection({
-  host: '127.0.0.1', // 数据库IP地址
+let dbTest = mysql.createPool({
+  // 崇明
+  limit: 100,
+  host: '112.64.134.154', // 数据库IP地址
   user: 'root', // 数据库登录账号
-  password: 'xiehou', // 数据库登录密码
-  database: 'persona_test' // 要操作的数据库
+  password: 'Ecust2023', // 数据库登录密码
+  database: 'persona_test', // 要操作的数据库
+  port: '3308',
+  timezone: 'Asia/Shanghai'
 })
 
-module.exports = db
+module.exports = dbTest
